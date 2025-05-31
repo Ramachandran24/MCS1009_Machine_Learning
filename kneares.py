33eee62f4f7c351b045bfc35bf6a9cb93cf177ea
ramachandranv24 <ramachandranmecse@gmail.com>
2025-05-31 14:41:52 +0530

    k

--- kneares.py
+++ kneares.py
@@ -2,7 +2,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 
-X = [[1, 2], [2, 3], [3, 1], [6, 5], [7, 8], [8, 6]]
+X = [[1, 2], [2, 3], [3, 1], [6, 7], [7, 8], [8, 6]]
 y = [0, 0, 0, 1, 1, 1]
 
 X_train, X_test, y_train, y_test = train_test_split(
@@ -11,7 +11,5 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 model = KNeighborsClassifier(n_neighbors=1)
 model.fit(X_train, y_train)
-
 pred = model.predict(X_test)
-
 print("Confusion Matrix:\n", confusion_matrix(y_test, pred))
